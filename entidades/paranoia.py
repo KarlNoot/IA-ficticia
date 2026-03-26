b368d7b8e17aac22e6aa70c65d355de21f35a511
Zefirká <rasconm118@gmail.com>
2026-03-26 10:30:38 -0700

    Agregue 1.1 clase y exception medio desarrollado

---
+++ entidades/paranoia.py
@@ -0,0 +1,22 @@
+"""Definineremos la clase de este robot todo paranoicop a la bestia
+aqui se definira su clase y funcionamiento"""
+
+from entidades.paranoiaerror import ParanoiaError
+
+class Paranoia:
+
+    def __init__(self, nombre: str, color: str, prompt: str):
+        self.nombre = nombre
+        self.color = color
+        self.prompt = prompt
+
+    def saludar(self) -> str:
+        if self.prompt != 'Q royo, no soy chat gipiti':
+            raise ParanoiaError("No we")
+        
+    
+        
+        return f"Hola, {self.nombre}, mucho gusto {"usuario"}"
+    
+def validar_mensaje(prompt):
+    
---
+++ entidades/paranoiaerror.py
@@ -0,0 +1,11 @@
+"""Crearemos exceptions para esta IA definiendolas como clases"""
+
+class ParanoiaError(Exception):
+
+    def __init__(self, mensaje: str):
+        self.mensaje = mensaje
+        super().__init__(mensaje)
+    
+    def __init__(self, color: str):
+        self.color = color
+        super().__init__(color)
